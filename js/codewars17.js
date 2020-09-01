c8fe42a07872344083ce11f626195c1e698fdd9a
kathyle-dev <69046536+kathyle-dev@users.noreply.github.com>
2020-09-01 19:25:30 -0400

    added codewar17 for 9/01

---
+++ js/codewars17.js
@@ -0,0 +1,30 @@
+// Build Tower
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+//
+// Tower block is represented as *
+//
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Lua: returns a Table;
+// Have fun!
+
+function towerBuilder(nFloors) {
+  let arr = [];
+  let padding = "";
+  for(let i = nFloors; i > 0; i--) {
+    let pushArr = '*';
+    for(let star = i-1; star > 0; star--) {
+      pushArr += "**";
+    }
+    pushArr = padding + pushArr + padding;
+    arr.unshift(pushArr);
+    padding += " ";
+  }
+  return arr;
+}
