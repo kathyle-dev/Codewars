97f04bed90e3d9ec814b3242dc922bef86a9512c
kathyle-dev <69046536+kathyle-dev@users.noreply.github.com>
2020-09-17 23:55:52 -0400

    added codwar29 for 9/17

---
+++ js/codewar29.js
@@ -0,0 +1,13 @@
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+//
+//     At least six characters long
+//     contains a lowercase letter
+//     contains an uppercase letter
+//     contains a number
+//
+// Valid passwords will only be alphanumeric characters.
+PREP --
+
+function validate(password) {
+  return /^[A-Za-z0-9]{6,}/.test(password);
+}
