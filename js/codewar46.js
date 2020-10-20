8a7c2defe13d6a1c9361f4d9e2602701c521ea31
kathyle-dev <69046536+kathyle-dev@users.noreply.github.com>
2020-10-19 21:02:40 -0400

    adding codewars 46-48 on 10.19

---
+++ js/codewar46.js
@@ -0,0 +1,14 @@
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+//
+// There will only be one 'longest' word.
+
+var spl = str.split(" ");
+var longest = 0
+
+for (var i = 0; i < spl.length; i++) {
+  if (spl[i].length >= longest) {
+    longest = spl[i].length
+    }
+  }
+  return longest
+}
